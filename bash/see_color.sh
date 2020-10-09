e0c386d63df8ffb1286ac99e09790ab244200c8d
Sean Campbell <sean.campbell13@gmail.com>
2020-10-09 17:33:10 -0400

    Add see_color.sh script to print color vars

---
+++ bash/see_color.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+source /home/natac/projects/dotfiles/bash/colors.sh
+
+# Regular Colors
+printf "${Black}Black\\n"
+printf "${Red}Red\\n"
+printf "${Green}Green\\n"
+printf "${Yellow}Yellow\\n"
+printf "${Blue}Blue\\n"
+printf "${Purple}Purple\\n"
+printf "${Cyan}Cyan\\n"
+printf "${White}White\\n"
+
+# Bold
+printf "${BBlack}BBlack\\n"
+printf "${BRed}BRed\\n"
+printf "${BGreen}BGreen\\n"
+printf "${BYellow}BYellow\\n"
+printf "${BBlue}BBlue\\n"
+printf "${BPurple}BPurple\\n"
+printf "${BCyan}BCyan\\n"
+printf "${BWhite}BWhite\\n"
+
+# Underline
+printf "${UBlack}UBlack\\n"
+printf "${URed}URed\\n"
+printf "${UGreen}UGreen\\n"
+printf "${UYellow}UYellow\\n"
+printf "${UBlue}UBlue\\n"
+printf "${UPurple}UPurple\\n"
+printf "${UCyan}UCyan\\n"
+printf "${UWhite}UWhite\\n"
+
+# Background
+printf "${On_Black}On_Black${Color_Off}\\n"
+printf "${On_Red}On_Red${Color_Off}\\n"
+printf "${On_Green}On_Green${Color_Off}\\n"
+printf "${On_Yellow}On_Yellow${Color_Off}\\n"
+printf "${On_Blue}On_Blue${Color_Off}\\n"
+printf "${On_Purple}On_Purple${Color_Off}\\n"
+printf "${On_Cyan}On_Cyan${Color_Off}\\n"
+printf "${On_White}On_White${Color_Off}\\n"
+
+# High Intensity
+printf "${IBlack}IBlack\\n"
+printf "${IRed}IRed\\n"
+printf "${IGreen}IGreen\\n"
+printf "${IYellow}IYellow\\n"
+printf "${IBlue}IBlue\\n"
+printf "${IPurple}IPurple\\n"
+printf "${ICyan}ICyan\\n"
+printf "${IWhite}IWhite\\n"
+
+# Bold High Intensity
+printf "${BIBlack}BIBlack\\n"
+printf "${BIRed}BIRed\\n"
+printf "${BIGreen}BIGreen\\n"
+printf "${BIYellow}BIYellow\\n"
+printf "${BIBlue}BIBlue\\n"
+printf "${BIPurple}BIPurple\\n"
+printf "${BICyan}BICyan\\n"
+printf "${BIWhite}BIWhite\\n"
+
+# High Intensity backgrounds
+printf "${On_IBlack}On_IBlack${Color_Off}\\n"
+printf "${On_IRed}On_IRed${Color_Off}\\n"
+printf "${On_IGreen}On_IGreen${Color_Off}\\n"
+printf "${On_IYellow}On_IYellow${Color_Off}\\n"
+printf "${On_IBlue}On_IBlue${Color_Off}\\n"
+printf "${On_IPurple}On_IPurple${Color_Off}\\n"
+printf "${On_ICyan}On_ICyan${Color_Off}\\n"
+printf "${On_IWhite}On_IWhite\\n${Color_O${Color_Off}ff}"
+
+
