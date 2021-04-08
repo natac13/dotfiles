56ce59b29809d267c6c6d72d002d33f9c6e00c82
Sean Campbell <sean.campbell13@gmail.com>
2021-04-08 11:39:40 -0400

    feat: mac setup from Harness Work Mini

---
+++ mac-setup.md
@@ -0,0 +1,19 @@
+# Mac Setup
+
+https://sourabhbajja.com/mac-setup/Homebrew/
+
+`sude xcode-select --install`
+
+## Install Brew
+
+This will handle all package installs like `apt` as well as much more, as I am finding out!!
+
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+Path will be updated automatically
+
+In a new termianl run `brew doctor`
+
+## Use `brew install --cask` for applications
+
+`brew install --cask alfred appcleaner docker google-chrome vlc zoom iterm2 etc.`
---
+++ zsh/plugins/tools/tools.plugin.zsh
@@ -0,0 +1,85 @@
+#####################################################
+# AWS
+#####################################################
+
+alias daws='docker container run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli'
+alias aws-shell='docker run -v ~/.aws:/root/.aws -it pahud/aws-shell'
+
+#####################################################
+# Tools
+#######################################################
+alias nivm=nvim
+
+alias bcryptToolContainer='docker container run --rm epicsoft/bcrypt'
+
+# alias ydl="youtube-dl"
+
+#####################################################
+# General
+#####################################################
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../../"
+alias b="cd -"
+alias his='history'
+
+# Added by Natac13
+alias gcom='git checkout $(git_main_branch)'
+alias gcm='git commit -m'
+alias gs='git status'
+# END added by Natac13
+# Aliases from oh my zsh git plugin that I am overriding or do not need.
+# alias gcmsg='git commit -m'
+# alias gcm='git checkout $(git_main_branch)'
+
+
+#####################################################
+# NNN
+#####################################################
+alias nnn='nnn -e'
+
+export NNN_FIFO='/tmp/nnn.fifo'
+# Block Device
+# Char Device
+# Directory
+# Exectutable
+# Regular
+# Hard Link
+# Symbolic link
+# Missing or file details
+# Orphaned symbolic link
+# FIFO
+# Socket
+# Unknown or 0b regular/exe
+export NNN_FCOLORS='c1e2512e006033f7c6d6abc4'
+export NNN_PLUG='f:finder;o:fzopen;c:fzcd;g:-_git diff;'
+# alias nnn='nn'
+
+nn ()
+{
+    # Block nesting of nnn in subshells
+    if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
+        echo "nnn is already running"
+        return
+    fi
+
+    # The default behaviour is to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # To cd on quit only on ^G, remove the "export" as in:
+    NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    # NOTE: NNN_TMPFILE is fixed, should not be modified
+    # export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    nnn -d "$@"
+
+    if [ -f "$NNN_TMPFILE" ]; then
+            . "$NNN_TMPFILE"
+            rm -f "$NNN_TMPFILE" > /dev/null
+    fi
+}
